31dcf6c3cba2ac5e2a4e00ecd3d4860c804266f5
hexu <hexu_1985@sina.com>
2021-12-30 16:51:02 +0800

    update binary_expansion.py

--- binary_expansion/recipe-01/binary_expansion.py
+++ binary_expansion/recipe-01/binary_expansion.py
@@ -3,6 +3,8 @@
 def binary_expansion(n):
     """
     将十进制整数进行二进制展开
+    如果n的二进制展开为(a_{n-1}, ..., a_1, a_0)
+    result为[a_0, a_1, ..., a_{n-1}]
     """
     q = n
     k = 0
@@ -16,4 +18,8 @@ def binary_expansion(n):
 if __name__ == '__main__':
     test_list = [12345, 177130, 241]
     for i in test_list:
-        print(i)
+        print(i, ":", bin(i))
+        blist = binary_expansion(i)
+        blist.reverse()
+        print(blist)
+
